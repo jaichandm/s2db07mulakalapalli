56acb9ddb3aab98fe749c6a91c93eb1995343829
mjaichand <mjaichandms@gmail.com>
2022-04-12 15:45:00 -0500

    final part completed

--- controllers/car.js
+++ controllers/car.js
@@ -14,9 +14,25 @@ exports.car_list = async function (req, res) {
 exports.car_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Car detail: ' + req.params.id);
 };
-// Handle car create on POST.
-exports.car_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Car create POST');
+// Handle Car create on POST.
+exports.car_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new Car();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"car_brand":"Mustang GT", "car_color":"Lucid Red Pearl", "car_cost":37000}
+    document.car_brand = req.body.car_brand;
+    document.car_cost = req.body.car_cost;
+    document.car_color = req.body.car_color;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // Handle car delete form on DELETE.
 exports.car_delete = function (req, res) {
@@ -29,13 +45,13 @@ exports.car_update_put = function (req, res) {
 
 // VIEWS
 // Handle a show all view
-exports.car_view_all_Page = async function(req, res) {
-    try{
-    theCars = await Car.find();
-    res.render('cars', { title: 'Car Search Results', results: theCars });
+exports.car_view_all_Page = async function (req, res) {
+    try {
+        theCars = await Car.find();
+        res.render('cars', { title: 'Car Search Results', results: theCars });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
 };
