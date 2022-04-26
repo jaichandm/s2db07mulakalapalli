c89acccfacb2a309d1f68489e4359baf80e427da
mjaichand <mjaichandms@gmail.com>
2022-04-25 22:24:52 -0500

    part 4 completed added protection to update endpoint

--- routes/cars.js
+++ routes/cars.js
@@ -8,15 +8,21 @@ router.get('/', function(req, res, next) {
   res.render('cars', { title: 'Search Results' });
 });
 */
-
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 /* GET costumes */
-router.get('/', car_controller.car_view_all_Page );
+router.get('/', car_controller.car_view_all_Page);
 /* GET detail car page */
 router.get('/detail', car_controller.car_view_one_Page);
 /* GET create car page */
 router.get('/create', car_controller.car_create_Page);
 /* GET create update page */
-router.get('/update', car_controller.car_update_Page);
+router.get('/update',secured, car_controller.car_update_Page);
 /* GET delete car page */
 router.get('/delete', car_controller.car_delete_Page);
 
